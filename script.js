d11111b188d443a8f4a0a0209d70963eb3b38e0a
Teise Fabian <dqm15teise@szut.uni-bremen.de>
2017-03-10 15:07:29 +0100

    changed from jcanvas to canvas

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
 $(document).ready(function() {
 
-$('canvas').drawLine({
-  strokeStyle: 10,
-  strokeWidth: 10,
-  x1: 1000, y1: 0,
-  x2: 1000, y2: 800,
-  x3: 0, y3: 800,
-  x4: 0, y4: 0,
-  x5: 1000, y5: 0,
-  
-});
-
-$('canvas').drawImage({
-  source: '../RocketGame/Images/Rocket.png',
-  x: 450, y: 700,
-  width: 80,
-  height: 100,
-  fromCenter: false,
-});
-
 
 });
