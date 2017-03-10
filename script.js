c6e04f4d5d3ca1c0480992b9b7671c9ee53ffcf9
Teise Fabian <dqm15teise@szut.uni-bremen.de>
2017-03-10 14:06:32 +0100

    borderforming

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
 
 $('canvas').drawLine({
   strokeStyle: 10,
-  strokeWidth: 20,
+  strokeWidth: 10,
   x1: 1000, y1: 0,
-  x2: 1000, y2: 1000,
-  x3: 0, y3: 1000,
+  x2: 1000, y2: 800,
+  x3: 0, y3: 800,
   x4: 0, y4: 0,
   x5: 1000, y5: 0,
   
