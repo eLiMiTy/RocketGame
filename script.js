53bf643b886c2fd39d1392683f016f60e9611d89
Teise Fabian <dqm15teise@szut.uni-bremen.de>
2017-03-10 14:02:56 +0100

    basic body

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+$('canvas').drawLine({
+  strokeStyle: 10,
+  strokeWidth: 20,
+  x1: 1000, y1: 0,
+  x2: 1000, y2: 1000,
+  x3: 0, y3: 1000,
+  x4: 0, y4: 0,
+  x5: 1000, y5: 0,
+  
+});
+
+});
